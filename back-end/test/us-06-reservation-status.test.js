d2f2afddb95ef4e5ebf79aa4a6eefd33f40461ea
William Mathis <wbmathi@g.clemson.edu>
2021-06-04 01:09:04 -0400

    final fix hopefully

--- back-end/test/us-06-reservation-status.test.js
+++ back-end/test/us-06-reservation-status.test.js
@@ -287,7 +287,8 @@ describe("US-06 - Reservation status", () => {
   });
 });
 
-function asDateString(date) {
+function asDateString(datee) {
+  let date = new Date(datee)
   return `${date.getFullYear().toString(10)}-${(date.getMonth() + 1)
     .toString(10)
     .padStart(2, "0")}-${date.getDate().toString(10).padStart(2, "0")}`;
